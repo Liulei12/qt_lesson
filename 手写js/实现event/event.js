c74eb7fae41c271b75aec3b053103b9941cedc7b
Liulei12 <1453421772@qq.com>
2019-12-06 18:52:08 +0800

    ''

---
+++ 手写js/实现event/event.js
@@ -0,0 +1,41 @@
+class EventEmitter {
+    constructor() {
+        this._events = this._events || new Map() //存储事件
+        this._maxListeners = this._maxListeners || 10 //监听上限
+    }
+}
+EventEmitter.prototype.emit = function (type, ...args) {
+    let handler;
+    // 从存储事件键值对的this.events中获取对应事件回调函数
+    handler = this._events.get(type)
+    if (Array.isArray(handler)) {
+        //有多个监听者
+        for (let i = 0; i < handler.length; i++) {
+            if (args.length > 0) {
+                handler[i].apply(this, args)
+            } else {
+                handler[i].apply(this)
+            }
+        }
+    } else {
+        if (args.length > 0) {
+            handler.apply(this, ...args)
+        } else {
+            handler.apply(this)
+        }
+    }
+    return true
+}
+// 监听名为type的事件
+EventEmitter.prototype.addListener = function (type, fn) {
+    const handler = this._events.get(type)//获取对应事件函数的清单
+    if (!handler) {
+        this._events.set(type, fn)
+    }
+    else if(handler && typeof handler === 'function'){
+        //只有一个监听者
+        this._events.set(type,[handler,fn])
+    }else{
+        handler.push(fn)
+    }
+}
---
+++ 手写js/深拷贝/copy.js
@@ -0,0 +1,65 @@
+const clone = parent => {
+    //判断类型
+    const isType = (obj,type) => {
+        if(typeof obj !== 'object') return false
+        const typeString = Object.prototype.toString.call(obj)
+        let flag;
+        switch (type) {
+            case 'Array':
+                flag = typeString === "[object Array]"
+                break;
+            case 'Date':
+                flag = typeString === "[object Date]"
+                break;
+            case 'RegExp':
+                flag = typeString === "[object RegExp]"
+                break;
+                default:
+                    flag = false
+        }
+        return flag
+    }
+    // 处理正则
+    const getRegExp = re => {
+        var flags = ''
+        if(re.global) flags += 'g';
+        if(re.ignoreCase) flags += 'i'
+        if(re.multiline) flags += 'm'
+        return flags
+    }
+
+    //循环引用的数据类型
+    const parents = []
+    const children = []
+
+    const _clone = parent => {
+        if (parent === null) return null
+        if(typeof parent !== 'object') return parent
+        let child, proto;
+
+        if (isType(parent),'Array') {
+            child = []
+        }else if (isType(parent,'RegExp')) {
+            child = new RegExp(parent.source,getRegExp(parent))
+            if(parent.lastIndex) {
+                child.lastIndex = parent.lastIndex
+            }
+        }else if(isType(parent,'Date')){
+            child = new Date(parent.getTime())
+        }else{
+            proto = Object.getPrototypeOf(parent)
+            child = Object.create(proto)
+        }
+        const index = parents.indexOf(parent)
+        if(index != -1){
+            return children[index]
+        }
+        parents.push(parent)
+        children.push(child)
+        for(let i in parent){
+            child[i] = _clone(parent[i])
+        }
+        return child;
+    }
+    return _clone(parent)
+}
